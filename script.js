4cf46386f276f17bd9060eae9230db78dce2cb34
revitalk <revital.kaisar@gmail.com>
2018-03-13 12:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AIzaSyANGAQenSvvkaFhDy-_pdwN0oiBt58EV6I&language=' + lang;
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=AddYourOwnKey&language=' + lang;
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
@@ -43,19 +43,16 @@ function plotMarkers(m)
           content: marker.description
     }); 
 
-
     
-    marker= new google.maps.Marker({
-        position: position,
-        map: map,
-        animation: google.maps.Animation.DROP
-      })
-      marker.addListener('click', function() {
+    marker =  new google.maps.Marker({
+              position: position,
+              map: map,
+              animation: google.maps.Animation.DROP
+              })
+    marker.addListener('click', function() {
     infowindow.open(map, marker);
-  });
+    });
   
-
-
     
     bounds.extend(position);
   });
